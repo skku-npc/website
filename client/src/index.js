d76e72d28694eb293672d328df6103f9e5d0e247
Jinu-Lee <jinulee@kakao.com>
2021-01-27 21:44:26 +0900

    Change bootstrap css file

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
