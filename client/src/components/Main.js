e742a811b8b4db5cdcc90e55271aae165c4fcc49
Jinu-Lee <jinulee@kakao.com>
2021-01-27 20:18:20 +0900

    Fix typo

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = () => {
+const Main = () => {
 	return (
 		<div>
 			<h2>메인</h2>
@@ -8,4 +8,4 @@ const Header = () => {
 	);
 };
 
-export default Header;
+export default Main;
