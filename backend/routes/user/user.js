e70e101b8c8521e39042a7a6eb61de1202ef3b00
threedalpeng <threedalpeng@gmail.com>
2021-01-30 17:51:14 +0900

    Use sendFile for static serve

--- backend/routes/user/user.js
+++ backend/routes/user/user.js
@@ -1,7 +1,7 @@
 const prisma = require('../../lib/prisma');
 
 async function sampleFunction(req, res) {
-  let user = await prisma.user.findFirst({
+  const user = await prisma.user.findFirst({
     where: {
       name: 'PSLover',
     },
