05bbe4a5a8edf7bf30de8343317660f354e1f48c
heukma <jony2000@g.skku.edu>
2021-01-31 22:12:36 +0900

    npc calendar

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NPCcalendar from './NPCcalendar'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NPCcalendar />
   </React.StrictMode>,
   document.getElementById('root')
 );
