88de96bac9cbc71362f3bc7e795c60d936cfe72d
Jinu-Lee <jinulee@kakao.com>
2021-02-01 23:22:16 +0900

    Fix Typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import Intro from './components/Intro/Intro';
+import Intro from './components/Intro';
 
 const App = () => {
   return (
