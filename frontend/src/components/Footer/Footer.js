d3a9f912d4a7b44afcf610b741c256154e3b2897
Jinu-Lee <jinulee@kakao.com>
2021-02-01 17:36:27 +0900

    refactor Header.js Footer.js

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return(
+    <div className="footer">
+      <div className="container-fluid no-pad">
+        <div className="row justify-content-around align-items-center">
+          <div className="col-4 no-pad" style={{margin:'3vh 0'}}>
+            <div className="row">
+              <div className="col-2 no-pad horizontal-center">
+                <img src="/icons/mail.png" alt="mailIcon"/>
+              </div>
+              <div className="col-10 no-pad">
+                <a href={'mailto:npc.skku@g.skku.edu'} target="_blank" rel="noopener noreferrer">
+									npc.skku@g.skku.edu
+                </a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-2 no-pad horizontal-center">
+                <img src="/icons/github.png" alt="githubIcon"/>
+              </div>
+              <div className="col-10 no-pad">
+                <a href={'https://github.com'} target="_blank" rel="noopener noreferrer">
+								Github
+                </a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-2 no-pad horizontal-center">
+                <img src="/icons/baekjoon.png" alt="BJIcon"/>
+              </div>
+              <div className="col-10 no-pad">
+                <a href={'https://www.acmicpc.net'} target="_blank" rel="noopener noreferrer">
+								Baekjoon OJ
+                </a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-2 no-pad horizontal-center">
+                <img src="/icons/map.png" alt="mapIcon"/>
+              </div>
+              <div className="col-10 no-pad">
+							경기도 수원시 장안구 2066 성균관대학교 자연과학캠퍼스 반도체관 6층 400609호
+              </div>
+            </div>
+          </div>
+          <div className="col-4 no-pad" style={{margin:'3vh 0'}}>
+            <div className="row justify-content-end">
+              회장 &nbsp;&nbsp;&nbsp;&nbsp; 손병호 &nbsp;&nbsp;
+              <a href={'mailto:xxx@g.skku.edu'} target="_blank" rel="noopener noreferrer">xxx@g.skku.edu</a>
+            </div>
+            <div className="row justify-content-end">
+              부회장 &nbsp;&nbsp;박재성 &nbsp;&nbsp;
+              <a href={'mailto:xxx@g.skku.edu'} target="_blank" rel="noopener noreferrer">xxx@g.skku.edu</a>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-end" style={{paddingRight:'20px'}}>
+          © 2011-{new Date().getFullYear()} NPC Club
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-  const fixedText = 'fixed Text';
-  const whenNotFixed = 'fixed Text';
-  const [headerText, setHeaderText] = useState(whenNotFixed);
+  const [headerFixed, setHeaderFixed] = useState(false);
   useEffect(() => {
     const header = document.getElementById('myHeader');
     const sticky = header.offsetTop;
     const scrollCallBack = window.addEventListener('scroll', () => {
       if (window.pageYOffset > sticky) {
         header.classList.add('sticky');
-        if (headerText !== fixedText) {
-          setHeaderText(fixedText);
+        if (headerFixed !== true) {
+          setHeaderFixed(true);
         }
       } else {
         header.classList.remove('sticky');
-        if (headerText !== whenNotFixed) {
-          setHeaderText(whenNotFixed);
+        if (headerFixed !== false) {
+          setHeaderFixed(false);
         }
       }
     });
@@ -26,55 +24,31 @@ const Header = () => {
       window.removeEventListener('scroll', scrollCallBack);
     };
   });
-  const headerStyle={
-    width:'100%',
-    height:'auto',
-    zIndex:100,
-    background: 'rgba(255, 255, 255, 0.7)',
-    backdropFilter: 'blur(16px)',
-  };
-  const headerLogo={
-    display:'inline-block',
-    paddingLeft:'5%',
-    paddingRight:'35%',
-    width:'105px',
-    height:'85px',
-    zIndex:101,
-  };
-  const headerMenu={
-    display:'inline-block',
-    width:'10%',
-    zIndex:101,
-    fontSize:'20px',
-    textAlign:'center',
-    verticalAlign:'bottom',
-    paddingBottom:'32.5px',
-  };
-  const headerLink = {
-    color: 'inherit',
-    textDecoration: 'none'
-  };
+
   return (
-    <header id="myHeader" className="header" style= {headerStyle}>
-      <Link to="/">
-        <img className="HeaderNPCLogo" src="/icons/npc.png" style={headerLogo} alt="HeaderLogo" />
-      </Link>
-      <div className="headerMenu" style={headerMenu}>
-        <Link to="/records" style={headerLink}>기록</Link>
+    <div className="header" id="myHeader">
+      <div className="container-fluid no-pad">
+        <div className="row justify-content-between align-items-center horizontal-center">
+          <div className="col-2 mr-auto no-pad">
+            <Link to="/">
+              <img className="HeaderNPCLogo" src="/icons/npc.png" alt="HeaderLogo"/>
+            </Link>
+          </div>
+          <div className="col-1 no-pad">
+            <Link className="header-link" to="/members">멤버</Link>
+          </div>
+          <div className="col-1 no-pad">
+            <Link className="header-link" to="/calendar">일정</Link>
+          </div>
+          <div className="col-1 no-pad">
+            <Link className="header-link" to="/study">스터디</Link>
+          </div>
+          <div className="col-1 no-pad">
+            <Link className="header-link" to="/settings">설정</Link>
+          </div>
+        </div>
       </div>
-      <div className="headerMenu" style={headerMenu}>
-        <Link to="/members" style={headerLink}>멤버</Link>
-      </div>
-      <div className="headerMenu" style={headerMenu}>
-        <Link to="/calendar" style={headerLink}>일정</Link>
-      </div>
-      <div className="headerMenu" style={headerMenu}>
-        <Link to="/study" style={headerLink}>스터디</Link>
-      </div>
-      <div className="headerMenu" style={headerMenu}>
-        <Link to="/settings" style={headerLink}>설정</Link>
-      </div>
-    </header>
+    </div>
   );
 };
 
