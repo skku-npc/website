245c205e6d5441efb7a473f465d5a76f125df997
kangrnin <kangrnin@gmail.com>
2021-02-04 21:46:51 +0900

    Add MemberList to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import Members from './components/Members';
+import MemberList from './components/MemberList';
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Header />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/members" component={Members} />
+          <Route exact path="/members" component={MemberList} />
           {/* 기타 페이지 */}
           <Route
             render={({ location }) => (
