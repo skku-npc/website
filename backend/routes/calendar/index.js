fd48f149abf13592fbab957531ebb303c7ccea32
김정원 <threedalpeng@gmail.com>
2021-02-07 19:17:16 +0900

    Adjust auth and admin middlewares

--- backend/routes/calendar/index.js
+++ backend/routes/calendar/index.js
@@ -5,11 +5,12 @@ const {
   removeEventById,
   createNewEvent,
 } = require('./event');
+const { auth, admin } = require('../../middleware/auth');
 const router = new express.Router();
 
 router.get('/events', getEvents);
-router.post('/event', createNewEvent);
-router.patch('/event/:eventId', updateEventById);
-router.delete('/event/:eventId', removeEventById);
+router.post('/event', [auth, admin], createNewEvent);
+router.patch('/event/:eventId', [auth, admin], updateEventById);
+router.delete('/event/:eventId', [auth, admin], removeEventById);
 
 module.exports = router;
