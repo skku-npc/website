86a7486fb99d487fc4f8145a935641dfbd5e2af2
uniqueimaginate <uniqueimaginate@gmail.com>
2021-02-10 19:38:48 +0900

    Handle CORS problem

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,9 +10,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/user', require('./routes/user/index'));
-
 app.use(express.static(path.join(path.resolve(), 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(path.resolve(), 'build', 'index.html'));
