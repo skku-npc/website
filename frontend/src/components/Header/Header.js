549489852e21e814b77197878c41426722d0f129
Jinu-Lee <jinulee@kakao.com>
2021-02-20 16:35:05 +0900

    Header 수정

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,22 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Login from '../Login';
 import './Header.css';
 
-const Header = ({ setLoginOpen, isLoggedIn, setIsLoggedIn }) => {
+const Header = ({ setModalContent, setModalOpen, isLoggedIn, setIsLoggedIn }) => {
   const [ dropdownOpen, setDropdownOpen ] = useState(false);
+  const dropdownEl = useRef();
+  const history = useHistory();
+
+  const clickOutside = ({ target }) => {
+    if (dropdownOpen && dropdownEl.current && !dropdownEl.current.contains(target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', clickOutside);
+    return () => {
+      window.removeEventListener('click', clickOutside);
+    };
+  }, [dropdownOpen]);
 
   const loginonClick = () => {
     if (isLoggedIn) {
       axios.post('http://localhost:4000/api/user/logout'
       ).then(() => {
+        delete axios.defaults.headers.common['Authorization'];
         setIsLoggedIn(false);
+        history.push('/');
       }).catch(error => {
         window.alert(error);
       });
     } else {
-      setLoginOpen(true);
+      setModalContent(<Login setModalOpen={setModalOpen} setIsLoggedIn={setIsLoggedIn} />);
+      setModalOpen(true);
     }
   };
 
@@ -43,9 +62,16 @@ const Header = ({ setLoginOpen, isLoggedIn, setIsLoggedIn }) => {
         </div>
         <div className="dropdown col-md-1 p-0 my-3 m-md-0">
           <img className="dropbtn" src="/icons/user.png" alt="user" onClick={() => setDropdownOpen(!dropdownOpen)}/>
-          <div className="dropdown-content" style={{display: dropdownOpen ? 'block' : 'none'}}>
+          <div className="dropdown-content" ref={dropdownEl} style={{display: dropdownOpen ? 'block' : 'none'}}>
             <a onClick={loginonClick}>{isLoggedIn ? '로그아웃' : '로그인'}</a>
-            <Link to="/settings">설정</Link>
+            <Link to="/settings" onClick={(event) => {
+              if (!isLoggedIn) {
+                event.preventDefault();
+                window.alert('로그인을 먼저 해주세요!');
+              } else {
+                setDropdownOpen(false);
+              }
+            }}>설정</Link>
           </div>
         </div>
       </div>
@@ -54,7 +80,8 @@ const Header = ({ setLoginOpen, isLoggedIn, setIsLoggedIn }) => {
 };
 
 Header.propTypes = {
-  setLoginOpen: PropTypes.func.isRequired,
+  setModalContent: PropTypes.func.isRequired,
+  setModalOpen: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
   setIsLoggedIn: PropTypes.func.isRequired
 };
